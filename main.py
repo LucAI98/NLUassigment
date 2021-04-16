d73e2335d01748894c28e21dc20147facd1e679a
LucAI98 <lucadanilo.melis@studenti.unitn.it>
2021-04-16 18:27:03 +0200

    extraction subtree extraction paths

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def print_hi(name):
     nlp = spacy.load('en_core_web_sm')
 
     piano_text = 'Gus is learning piano with his teacher'
-    piano_doc = nlp(piano_text)
+    extraction_paths(piano_text)
+    """piano_doc = nlp(piano_text)
 
     for token in piano_doc:
         print(token.text, token.tag_, token.head.text, token.dep_)
@@ -21,21 +22,38 @@ def print_hi(name):
 
     # extractiom_root_token(root, piano_doc[6])
     # http://127.0.0.1:5000
-    # displacy.serve(piano_doc, style='dep')
+    # displacy.serve(piano_doc, style='dep')"""
 
 
 # extract a path of dependency relations from the ROOT to a token
-def extractiom_root_token(root, token):
+def extractiom_root_token1(root, token, path):
     if root == token:
         print(root.text + '(' + root.dep_ + ')', end=' ')
+        path.append(token)
     else:
-        extractiom_root_token(root, token.head)
-        print('->  ' + token.text + '(' + token.dep_ + ')', end=' ')
+        extractiom_root_token1(root, token.head, path)
+        print('->  ' + '(' + token.dep_ + ')  ' + token.text, end=' ')
+        path.append(token)
+
+
+def extraction_paths(sentece):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(sentece)
+
+    paths = []
+    root = search_root(doc)
+    for token in doc:
+        path = []
+        extractiom_root_token1(root, token, path)
+        print(" ")
+        paths.append(path)
+
+    return paths
 
 
 # extract subtree of a dependents given a token
 def extraction_subtree(token):
-    return token.subtree
+    return list(token.subtree)
 
 # check if a given list of tokens (segment of a sentence) forms a subtree
 
